0bb20db811f0dfc7eda5d86563dd7ad53ffa10a5
Everton Gonçalves <dsgeverton@gmail.com>
2018-05-21 20:32:09 -0300

    WIP

--- app/src/main/java/br/edu/iff/pooa20181/calculoimc/MainActivity.java
+++ app/src/main/java/br/edu/iff/pooa20181/calculoimc/MainActivity.java
@@ -2,6 +2,7 @@ package br.edu.iff.pooa20181.calculoimc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnCalcular;
+
+        btnCalcular = (Button) findViewById(R.id.btnCalcular);
+
+        btnCalcular.setOnClickListener();
+
     }
 }
